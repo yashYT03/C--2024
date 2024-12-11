f8641591b0a54c4f4a1547ca9b897ec80efb2c53
WHO AM I ? <114454845+yashYT03@users.noreply.github.com>
2024-12-11 11:59:07 +0530

    Add files via upload
    I am Writing this code only basic understanding if you want any problem please write a commit 💕

---
+++ AllCSHARP2/Who/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+class Who
+{
+    static void Main(String[] args)
+    {
+        //DataType in c#
+        /* int
+         * float
+         * char a = 'A';
+         * bool is true or false
+         * string
+         */
+        //string inp = Console.ReadLine();
+        //Console.WriteLine(inp);
+        //Console.WriteLine("hello world");
+        //int a = 10;
+        //Console.WriteLine("hello world" + a);
+        //Console.ReadLine();
+
+        //int a = 10;
+        //float c = 20.6F;
+        //double t = 1.1D;
+        //char f = 'a';
+        //string e = "we are anonymous";
+        //Console.WriteLine(a);
+        //Console.WriteLine(c);
+        //Console.WriteLine(t);
+        //Console.WriteLine(f);
+        //Console.WriteLine(e);
+        //Console.ReadLine();
+
+        //TYPECASTING
+        //1.Implicit Casting
+        //int x = 3;
+        //double y = x;
+        //2.Explicit Casting
+        //int a = (int) 3.5;
+        //Console.WriteLine(a);
+        //Console.WriteLine(y);
+        //Console.ReadLine();
+
+        //float a = Convert.ToInt64(3.4);
+        //Console.WriteLine(a);
+        //Console.ReadLine();
+
+        Console.WriteLine("We are anonymous");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("you can give vote" + " " + a);
+
+
+
+        
+        Console.ReadLine();
+
+    }
+}
+
